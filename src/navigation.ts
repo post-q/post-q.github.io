3d3dc307ecfc9df0e1a649d33f06cb92aa78eba2
blelump <blelump@martex.nom.pl>
2025-12-04 19:12:59 +0100

    fix: minor fixes

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,16 +3,16 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Blog',
-      href: getBlogPermalink(),
-    },
-    {
       text: 'Oferta',
       href: '/#oferta',
     },
     {
       text: 'Kontakt',
       href: '/#kontakt',
+    },
+    {
+      text: 'Blog',
+      href: getBlogPermalink(),
     },
   ],
   actions: [],
